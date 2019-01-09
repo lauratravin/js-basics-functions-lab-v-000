58750a706dee8edcfb8977d3685ce799c31ac31c
Laura Messa Travin <laura.travin@gmail.com>
2019-01-09 23:53:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const blocksInFeet = 264
+const locationValue = 42
+function distanceFromHqInBlocks(passengerLocation){
+
+  let distanceValue = locationValue > passengerLocation ?  (locationValue-passengerLocation):(passengerLocation-lovationValue);
+  return distanceValue;
+}
+function distanceFromHqInFeet(){
+
+}
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+
+}

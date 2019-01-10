df271b809780908872d729459dae369e478e140e
Laura Messa Travin <laura.travin@gmail.com>
2019-01-10 00:44:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ function calculatesFarePrice(start, destination){
 
   if (travelFeet > 2500) {
      return  "cannot travel that far";
-  } else if (2000 <  travelFeet &&  travelFeet <= 2500 ){
-    
+  } else if (2000 <  travelFeet &&  travelFeet <= 2500 ){  
     price = 25;
-    return price;
+    
   } else if(400 <=  travelFeet &&  travelFeet <= 2000 ){
      price = (travelFeet - 400)* 2;
-    return price;
+  
  } else if(travelFeet < 400 ){
     price = 0;
-   return price;
+   
 }
+return price;
 
 
 
